69de2abcc1b6a2db1cffb2ec152c45ab04a7cf40
Viet Le <vietleuit@gmail.com>
2024-06-14 15:32:11 +0700

    Revert logic login API

--- routers/auth.py
+++ routers/auth.py
@@ -1,13 +1,13 @@
 from datetime import timedelta, datetime, timezone
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, Body
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
 from enum import Enum
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 
 from database import SessionLocal
@@ -71,11 +71,6 @@ class CreateUserRequest(BaseModel):
         }
 
 
-class LoginRequest(BaseModel):
-    username: str
-    password: str
-
-
 def authenticate_user(username: str, password: str, db):
     user = db.query(Users).filter(Users.username == username).first()
     if not user:
@@ -128,8 +123,8 @@ async def create_user(db: db_dependency, new_user: CreateUserRequest):
 
 
 @router.post('/token', name='Generate token when user login', response_model=Token)
-async def login_for_access_token(db: db_dependency, login_request: LoginRequest = Body(...)):
-    current_user = authenticate_user(login_request.username, login_request.password, db)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    current_user = authenticate_user(form_data.username, form_data.password, db)
     if not current_user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Could not validate user')
